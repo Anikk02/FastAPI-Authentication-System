f6eb66c6ea34341bffe4e6b4970c1b6c7421e0ff
Anikk02 <kiddoanyy0208@gmail.com>
2026-05-24 04:11:38 +0530

    Remove user cache completely, already handled by the session data

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -3,15 +3,10 @@ import hashlib
 
 from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
 
 from app.core.security import decode_access_token
-from app.database import get_db
-from app.models.user import User
 from app.schemas import UserResponse
-
-from app.core.redis import redis_get, redis_set, redis_exists
+from app.core.redis import redis_get
 
 logger = logging.getLogger(__name__)
 security = HTTPBearer()
@@ -19,7 +14,6 @@ security = HTTPBearer()
 
 async def get_current_user(
     credentials: HTTPAuthorizationCredentials = Depends(security),
-    db: AsyncSession = Depends(get_db)
 ) -> UserResponse:
 
     try:
@@ -60,49 +54,8 @@ async def get_current_user(
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail="Session expired or revoked"
             )
-
-        # Step 2: USER CACHE
-
-        cache_key = f"user:{user_id}"
-        cached_user = await redis_get(cache_key)
-
-        # 1. Try Redis first
-        if cached_user:
-            logger.info(f"Cache HIT user_id={user_id}")
-            return UserResponse.model_validate_json(cached_user)
-
-        # 2. Fallback to DB
-        logger.info(f"Cache MISS user_id={user_id}")
-        result = await db.execute(
-            select(User).where(User.id == user_id)
-        )
-        user = result.scalar_one_or_none()
-
-        if user is None:
-            logger.warning(f"User not found user_id={user_id}")
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="User not found"
-            )
-
-        if not user.is_active:
-            logger.warning(f"Inactive user user_id={user_id}")
-            raise HTTPException(
-                status_code=status.HTTP_403_FORBIDDEN,
-                detail="Inactive user account"
-            )
-
-        # 3. Convert to schema
-        user_response = UserResponse.model_validate(user)
-
-        # 4. Store in Redis (TTL = 30 minutes)
-        await redis_set(
-            cache_key,
-            user_response.model_dump_json(),
-            ttl=1800 #30 minutes
-        )
-
-        return user_response
+        
+        return UserResponse.model_validate_json(session_valid)
 
     except HTTPException:
         raise
